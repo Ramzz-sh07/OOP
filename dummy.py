58bbf79eee7e5745de4ff460d87c48021add6304
Ramya Shree B <ramyashreeb@Ramyas-MacBook-Air.local>
2026-04-21 22:15:00 +0100

    This is a perfect example of a "Secure System Design." It shows you understand how to protect data (Encapsulation) and how to manage shared resources (Static variables)

--- dummy.py
+++ dummy.py
@@ -1,34 +1,99 @@
-class Chatbook:
-    # THE CONSTRUCTOR: Initializes the user's data
+class chatbook:
+    # --- STATIC VARIABLE (Shared by all users) ---
+    __user_id = 1
+
     def __init__(self):
+        # 1. Assign unique ID and increment the global counter
+        self.id = chatbook.__user_id
+        chatbook.__user_id += 1
+        
+        # 2. Private attribute (The 'Locked Box')
+        self.__name = "Default User"
+        
+        # 3. Instance attributes
         self.username = ''
         self.password = ''
         self.loggedin = False
-        # Automatically trigger the menu when an object is created
-        self.menu()
+        
+        # 4. THE TRIGGER: This starts the program automatically!
+        self.menu() 
 
-    # THE MENU METHOD: The control center for the application
+    # --- STATIC METHODS (Class-level logic) ---
+    @staticmethod
+    def get_id():
+        return chatbook.__user_id
+    
+    @staticmethod
+    def set_id(val):
+        chatbook.__user_id = val
+
+    # --- GETTER & SETTER (Accessing the Locked Box) ---
+    def get_name(self):
+        return self.__name
+    
+    def set_name(self, value):
+        self.__name = value
+
+    # --- THE ACTIONS (Methods) ---
     def menu(self):
         user_input = input("""
-        Welcome to Chatbook!! How would you like to proceed?
-        1. Press 1 to signup
-        2. Press 2 to signin
-        3. Press 3 to write a post
-        4. Press 4 to message a friend
-        5. Press any other key to exit
-        """)
-
+Welcome to Chatbook !! How would you like to proceed?
+1. Press 1 to signup
+2. Press 2 to signin
+3. Press 3 to write a post
+4. Press 4 to message a friend
+5. Press any other key to exit
+-> """)
         if user_input == "1":
-            pass # Placeholder for signup logic
+            self.signup()
         elif user_input == "2":
-            pass # Placeholder for signin logic
+            self.signin()
         elif user_input == "3":
-            pass # Placeholder for posting logic
+            self.my_post()
         elif user_input == "4":
-            pass # Placeholder for messaging logic
+            self.sendmsg()
         else:
             exit()
 
-# --- TRIGGERING THE CODE ---
-# This creates the object and immediately launches the menu
-obj = Chatbook()
+    def signup(self):
+        email = input("enter your email here -> ")
+        pwd = input("setup your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("✅ You have signed up successfully !!\n")
+        self.menu()
+
+    def signin(self):
+        if self.username == '' and self.password == '':
+            print("❌ Please signup first by pressing 1 in the main menu")
+        else:
+            uname = input("enter your email/username here -> ")
+            pwd = input("Enter your password here -> ")
+            if self.username == uname and self.password == pwd:
+                print("🌟 You have signed in successfully !!")
+                self.loggedin = True
+            else:
+                print("❌ Incorrect credentials..")
+        print("\n")
+        self.menu()
+
+    def my_post(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here -> ")
+            print(f"📝 Posted: {txt}")
+        else:
+            print("🛑 You need to signin first to post!")
+        self.menu()
+
+    def sendmsg(self):
+        if self.loggedin == True:
+            txt = input("Enter your message here -> ")
+            frnd = input("Whom to send the msg? -> ")
+            print(f"📧 Message sent to {frnd}")
+        else:
+            print("🛑 You need to signin first to send messages!")
+        self.menu()
+
+# --- THE SPARK ---
+# Creating 'user1' now triggers the __init__ which triggers the menu!
+user1 = chatbook()
